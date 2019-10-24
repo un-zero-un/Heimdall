ccc5c5fd77e5a70498f31973f1065cc5563faba7
Yohan Giarelli <yohan@un-zero-un.fr>
2019-10-24 16:42:14 +0200

    Fixed a side effect in DateDiff timeout

--- assets/js/common/components/DateDiff.tsx
+++ assets/js/common/components/DateDiff.tsx
@@ -1,5 +1,5 @@
 import moment from 'moment';
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 
 type Props = {
     date: Date | string,
@@ -9,7 +9,11 @@ export default function DateDiff({date}: Props) {
     const [state, setState] = useState<boolean>(false);
     const parsedDate        = date instanceof Date ? date : new Date(Date.parse(date));
 
-    setTimeout(() => setState(!state), 10000);
+    useEffect(() => {
+        const timeout = setTimeout(() => setState(!state), 10000);
+
+        return () => clearTimeout(timeout);
+    });
 
     return (
         <time dateTime={parsedDate.toISOString()}>
